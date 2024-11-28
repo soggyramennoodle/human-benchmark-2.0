7616bd57f1ed0989e82c992b89292a278a12c97e
soggyramennoodle <therealabeeb@icloud.com>
2024-11-28 12:16:49 -0600

    human benchmark 2.0
    start of major project

---
+++ sketch.js
@@ -0,0 +1,15 @@
+// human benchmark 2.0
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
