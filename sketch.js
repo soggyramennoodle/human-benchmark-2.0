3c8ba7b74ea69c605417636bfb643d89e57fe4d1
soggyramennoodle <therealabeeb@icloud.com>
2024-12-02 12:15:50 -0600

    Capstone Project
    Added grid to project, figuring out how to generate random sequence

--- sketch.js
+++ sketch.js
@@ -1,15 +1,51 @@
 // human benchmark 2.0
-// Your Name
-// Date
+// Adeeb Rahman
+// 12.2.2024
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// CAPSTONE PROJECT
 
+let NUM_ROWS = 4;
+let NUM_COLS = 4;
+let rectWidth, rectHeight;
+let currentRow, currentCol;
+let grid = [[0,0,0,0],
+                   [0,0,0,0],
+                   [0,0,0,0],
+                   [0,0,0,0]];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1000, 700);
+  rectWidth = width/NUM_COLS;
+  rectHeight = height/NUM_ROWS;
+  stroke(255);
 }
 
 function draw() {
-  background(220);
+  background(20);
+  drawGrid();
+  coordinate(); //testing coordinates of grid
 }
+
+function drawGrid() {
+  for (let x = 0; x < NUM_COLS; x++) {
+    for (let y = 0; y < NUM_ROWS; y++) {
+      fill(grid[y][x]);
+      rect(x*rectWidth, y*rectHeight, rectWidth, rectHeight);
+    }
+  }
+}
+
+function generateSequence() {
+  for ()
+}
+
+
+///////////////////////--------TESTING CORNER-----------//////////////////////////
+function coordinate() {
+  if (mouseX >=0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
+    let gridX = (mouseX / rectWidth);
+    let gridY = (mouseY / rectHeight);
+    console.log(gridX, gridY);
+  }
+}
+
