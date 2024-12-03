ee6ac7a19b80fdb136625f110dad12ca22fa5bb7
soggyramennoodle <therealabeeb@icloud.com>
2024-12-03 12:12:52 -0600

    Update sketch.js
    Created the basis of the function that generates a random sequence of squares (coordinates) from the grid

--- sketch.js
+++ sketch.js
@@ -13,17 +13,23 @@ let grid = [[0,0,0,0],
                    [0,0,0,0],
                    [0,0,0,0]];
 
+let sequence = []; //stores sequence in order
+let sequenceLength; //length of sequence, should increase with dififculty 
+
 function setup() {
   createCanvas(1000, 700);
   rectWidth = width/NUM_COLS;
   rectHeight = height/NUM_ROWS;
   stroke(255);
+  generateSequence(); //generates sequence, need to find way to put in draw, to add onto sequence
 }
 
 function draw() {
   background(20);
   drawGrid();
-  coordinate(); //testing coordinates of grid
+
+  // testCoordinate(); //testing coordinates of grid
+  
 }
 
 function drawGrid() {
@@ -36,16 +42,30 @@ function drawGrid() {
 }
 
 function generateSequence() {
-  for ()
+  sequence = []; //empty sequence at start, want generation in setup
+  sequenceLength = 4; ///set length for testing
+  for (let i = 0; i < sequenceLength; i ++) {
+    let randomCol = int(random(0, NUM_COLS));
+    let randomRow = int(random(0, NUM_ROWS));
+    sequence.push([randomCol, randomRow]);
+  }
+  console.log(sequence); //here for testing sequence generation
+}
+
+function highlightSequence() {
+  for (let i = 0; i < sequenceLength; i++) {
+    
+  }
 }
 
 
 ///////////////////////--------TESTING CORNER-----------//////////////////////////
-function coordinate() {
-  if (mouseX >=0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
-    let gridX = (mouseX / rectWidth);
-    let gridY = (mouseY / rectHeight);
-    console.log(gridX, gridY);
-  }
-}
+
+// function testCoordinate() { //used to test grid coordinate system
+//   if (mouseX >=0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
+//     let gridX = int((mouseX / rectWidth));
+//     let gridY = int((mouseY / rectHeight));
+//     console.log(gridX, gridY);
+//   }
+// }
 
